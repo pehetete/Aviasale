bdcf9f221c78fc4943a4313f816c82ca090c2e39
pehetete <58585465+pehetete@users.noreply.github.com>
2019-12-06 12:36:33 +0500

    Add files via upload

---
+++ Aviasale/FinalInfoForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+
+namespace Aviasale
+{
+    public partial class FinalInfoForm : Form
+    {
+        public FinalInfoForm() => InitializeComponent();
+
+        private void FinalInfoForm_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < PersonalInfoForm.Passenger.Count; i++)
+            {
+                ListViewItem item = new ListViewItem(PersonalInfoForm.Passenger[i][0].ToString());
+                item.SubItems.Add(PersonalInfoForm.Passenger[i][1].ToString());
+                item.SubItems.Add(PersonalInfoForm.Passenger[i][2].ToString());
+                item.SubItems.Add(PersonalInfoForm.Passenger[i][3].ToString());
+                item.SubItems.Add(PersonalInfoForm.Passenger[i][4].ToString());
+                item.SubItems.Add(PersonalInfoForm.Passenger[i][5].ToString());
+
+                listView1.Items.Add(item);
+            }
+            
+        }
+
+        private void btnCreateReport_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < PersonalInfoForm.Passenger.Count; i++)
+            {
+                var wordApp = new Microsoft.Office.Interop.Word.Application();
+                var wordDoc = wordApp.Documents.Add(System.Windows.Forms.Application.StartupPath + "\\Sig.dotx");
+
+                string FullName = $"{PersonalInfoForm.Passenger[i][0].ToString()} {PersonalInfoForm.Passenger[i][1].ToString()} {PersonalInfoForm.Passenger[i][2].ToString()}";
+                int Age = int.Parse(PersonalInfoForm.Passenger[i][3].ToString());
+                ReplaceStub("#Город", GetInfoForm.ToWhere, wordDoc);
+                ReplaceStub("#стоимость", PersonalInfoForm.Passenger[i][4].ToString(), wordDoc);
+                ReplaceStub("#ФИО", FullName, wordDoc);
+                ReplaceStub("#паспорт", PersonalInfoForm.Passenger[i][6].ToString(), wordDoc);
+                ReplaceStub("#дата", DateTime.Today.ToString(), wordDoc);
+                if (PersonalInfoForm.Passenger[i][4].ToString() == "0")
+                    ReplaceStub("#вид", "Бесплатный", wordDoc);
+                else if (Age >= 2 && Age <= 12)
+                    ReplaceStub("#вид", "Уцененный", wordDoc);
+                else ReplaceStub("#вид", "Платный", wordDoc);
+                ReplaceStub("#возраст", Age.ToString(), wordDoc);
+
+                wordDoc.SaveAs($"{System.Windows.Forms.Application.StartupPath}\\Билет №{i + 1} {FullName}.docx");
+                wordDoc.Close();
+                wordApp.Quit();
+            }
+        }
+
+        private void ReplaceStub(string stubToReplace, string text, Document worldDocument)
+        {
+            var range = worldDocument.Content;
+            range.Find.ClearFormatting();
+            object wdReplaceAll = WdReplace.wdReplaceAll;
+            range.Find.Execute(FindText: stubToReplace, ReplaceWith: text, Replace: wdReplaceAll);
+        }
+    }
+}
---
+++ Aviasale/PersonalInfoForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Aviasale
+{
+    public partial class PersonalInfoForm : Form
+    {
+        public PersonalInfoForm() => InitializeComponent();
+
+        internal static List<List<object>> Passenger = new List<List<object>>();
+        int Passangers = GetInfoForm.Adults + GetInfoForm.Kids,
+            PassangerCounter = 1;
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            int Age = DateTime.Now.Year - Convert.ToDateTime(txtBirthDate.Text).Year;
+            double Price = 0;
+            bool IsAdult = false;
+
+            if (Age < 2)
+                Price = 0;
+            else if (Age >= 2 && Age <= 12)
+                Price = GetInfoForm.Price * 0.5;
+            else if (Age > 12)
+            {
+                Price = GetInfoForm.Price;
+                IsAdult = true;
+            }
+
+            Passenger.Add(new List<object> { txtSecond.Text, txtFirst.Text, txtMiddle.Text, Age, Price, IsAdult, txtPassport.Text });
+
+            if (PassangerCounter < Passangers)
+            {
+                PassangerCounter++;
+                Text = "Пассажир №" + (PassangerCounter);
+            }
+            else
+            {
+                Hide();
+                FinalInfoForm finalInfoForm = new FinalInfoForm();
+                finalInfoForm.Show();
+            }
+        }
+
+        private void PersonalInfoForm_Load(object sender, EventArgs e)
+        {
+            Text = "Пассажир №1";
+        }
+    }
+}
